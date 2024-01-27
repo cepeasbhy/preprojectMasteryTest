b3a6ab9311912d07f8a2a1a95acc8d5436d3068b
cepeasbhy <asbhycepe01@gmail.com>
2024-01-27 09:41:20 +0800

    .

--- PojectMastery/Interfaces/IProductRepository.cs
+++ PojectMastery/Interfaces/IProductRepository.cs
@@ -10,13 +10,6 @@ public interface IProductRepository
     public Task<int> GetTotalSearchResult(string searchText);
     public Task<Product?> GetProductById<T>(T id);
     public Task<int> AddProduct(Product product);
-<<<<<<< Updated upstream
     public Task<int> UpdateProduct<T>(T id, Product product);
-=======
-<<<<<<< HEAD
-    
-=======
-    public Task<int> UpdateProduct<T>(T id, Product product);
->>>>>>> 5d3f44c8fe01715afd02542ae52684c20b6d26fa
->>>>>>> Stashed changes
+
 }
